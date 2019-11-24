ee31fa9c754f266a68acec47e2c7bd52db8b9783
muffin3 <god.coder3@gmail.com>
2019-11-24 09:33:47 +0300

    gui removed temporarily

--- app/main.py
+++ app/main.py
@@ -1,22 +1,7 @@
-import sys
-import atos
 from terminal import Terminal
-from PyQt5 import QtWidgets
-from gui.sign import SignWindow
-from gui.main import MainWindow
 from colorama import init
 
 if __name__ == "__main__":
-
-    atos = atos.Atos('os.txt')
-
-    if len(sys.argv) > 1 and sys.argv[1] == 'gui':
-        app = QtWidgets.QApplication([])
-        sign = SignWindow(atos)
-        sign.show()
-
-        sys.exit(app.exec())
-    else:
-        init()
-        terminal = Terminal('os.txt')
-        terminal.run()
+    init()
+    terminal = Terminal('os.txt')
+    terminal.run()
